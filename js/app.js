cccb7e5694fd3288a36fa40b40cdfd3a8459f633
Fabricio Alejandro Ortiz Fiallos <faortiz-fiallos+seneca@myseneca.ca>
2025-12-25 22:05:36 -0500

    changes into js,index and css

--- js/app.js
+++ js/app.js
@@ -3,7 +3,18 @@ function getId(id){
 }
 
 
-let dateID = document.getElementById("date");
+let dateElement = document.getElementById("date");
 
+const CurrentDate = new Date();
+const dateTimeformatter = new Intl.DateTimeFormat("en-US", {
+  month: "short",
+  day: "numeric",
+  hour: "numeric",
+  minute: "2-digit",
+  hour12: true
+});
+
+const formattedDate = dateTimeformatter.format(CurrentDate);
+dateElement.textContent = formattedDate;
 
 
