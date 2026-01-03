da85a02e3a391b340015ea9551d134406f7dc9c6
Fabricio Alejandro Ortiz Fiallos <faortiz-fiallos+seneca@myseneca.ca>
2026-01-03 17:19:00 -0500

    changes into css

--- js/app.js
+++ js/app.js
@@ -1,20 +1,3 @@
-function getId(id){
-  return document.getElementById(id);
-}
 
-
-let dateElement = document.getElementById("date");
-
-const CurrentDate = new Date();
-const dateTimeformatter = new Intl.DateTimeFormat("en-US", {
-  month: "short",
-  day: "numeric",
-  hour: "numeric",
-  minute: "2-digit",
-  hour12: true
-});
-
-const formattedDate = dateTimeformatter.format(CurrentDate);
-dateElement.textContent = formattedDate;
 
 
--- js/site-nav.js
+++ js/site-nav.js
@@ -1,17 +1,17 @@
 
 class SiteNav extends HTMLElement {
-    constructor() {
-        super();
-        this.attachShadow({mode: "open"});
-        this.render();
-    }
-    render(){
-        this.shadowRoot.innerHTML = `
+  constructor() {
+    super();
+    this.attachShadow({ mode: "open" });
+    this.timerId;
+  }
+  render() {
+    this.shadowRoot.innerHTML = `
     <link rel="stylesheet" href="/css/site-nav.css">
 
     <nav> 
       <div class="left-bar">
-        <img class="logo-control" src="assets/icon2.png" alt="Ana's logo">
+        <img class="logo-control" src="assets/logo2.png" alt="Ana's logo">
         <span class="date"></span>
       </div>
 
@@ -33,57 +33,58 @@ class SiteNav extends HTMLElement {
           <li>
             <a href="#" class="flip">
               <span class="flip-inner">
-                <span class="flip-face flip-front">Blog</span>
-                <span class="flip-face flip-back">Blog</span>
+                <span class="flip-face flip-front">About peaches</span>
+                <span class="flip-face flip-back">About peaches</span>
               </span>
             </a>
           </li>
 
           <li>
             <a href="#" class="flip">
-              <span class="flip-inner"> 
-                <span class="flip-face flip-front">Start a project</span>
-                <span class="flip-face flip-back">Start a project</span>
-              </span>
+              <span class="flip-inner">
+              <span class="flip-face flip-front" id="container">Start a project</span>
+              <span class="flip-face flip-back" id="container">Start a project</span>
+            </span>
             </a>
           </li>
         </ul>
       </div>
     </nav>
   `;
-}
+  }
 
-    connectedCallback() {
+  connectedCallback() {
+    this.render();
 
     this.dateEl = this.shadowRoot.querySelector(".date");
 
     this.formatted = new Intl.DateTimeFormat("en-US", {
-        month: "short",
-        day: "2-digit",
-        hour: "numeric",
-        minute: "2-digit",
-        hour12: true,
+      month: "short",
+      day: "2-digit",
+      hour: "numeric",
+      minute: "2-digit",
+      hour12: true,
 
     });
 
     this.updatedDate();
-   
+
     this.timerId = setInterval(() => this.updatedDate(), 60_000);
 
-    }
+  }
 
-    disconnectedCallback() {
+  disconnectedCallback() {
 
-        if(this.timerId){
-            clearInterval(this.timerId);
-            this.timerId = null;
-        }
+    if (this.timerId) {
+      clearInterval(this.timerId);
+      this.timerId = null;
     }
+  }
 
-    updatedDate(){
-        if(!this.dateEl) return;
-        this.dateEl.textContent = this.formatted.format(new Date());
-    }
+  updatedDate() {
+    if (!this.dateEl) return;
+    this.dateEl.textContent = this.formatted.format(new Date());
+  }
 
 
 }
